d646330f5f861f01ad6d25325793ce5c88c7555b
Barbara <barbarasanderstruth@gmail.com>
2019-06-12 00:05:17 +0000

    Splurty L15

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -16,7 +16,11 @@ class QuotesController < ApplicationController
     end
   end
   
+  def about
+  end
+  
   private
+  
   def quote_params
     params.require(:quote).permit(:saying, :author)
   end  
